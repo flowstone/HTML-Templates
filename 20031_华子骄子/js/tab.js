5e8b912ed6625dd481f72e6de94f77f4d4e5602a
flowstone <yaoxue2014@hotmail.com>
2016-12-26 23:54:09 +0800

    华子骄子-教育结构

---
+++ 20031_华子骄子/js/tab.js
@@ -0,0 +1,12 @@
+<!--
+/*第一种形式 第二种形式 更换显示样式*/
+function setTab(name,cursel,n){
+for(i=1;i<=n;i++){
+var menu=document.getElementById(name+i);
+var con=document.getElementById("con_"+name+"_"+i);
+menu.className=i==cursel?"hover":"";
+con.style.display=i==cursel?"block":"none";
+}
+}
+//-->
+/*一流素材网www.16sucai.com*/
